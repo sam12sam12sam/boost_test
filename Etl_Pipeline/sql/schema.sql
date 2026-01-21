d37f95feff9b2d6b4d0f09b66c19e961317b06d3
Sambireddy Puli <82464751+sam12sam12sam@users.noreply.github.com>
2026-01-22 00:12:18 +0530

    Update schema.sql

--- Etl_Pipeline/sql/schema.sql
+++ Etl_Pipeline/sql/schema.sql
@@ -2,9 +2,8 @@
 DROP SCHEMA IF EXISTS boost_test;
 CREATE SCHEMA boost_test;
 use boost_test;
-
 -- RAW CSV TABLE
--- DROP TABLE IF EXISTS raw_test;
+DROP TABLE IF EXISTS raw_test;
 CREATE TABLE raw_test (
     id VARCHAR(20),
     name VARCHAR(255),
@@ -17,6 +16,7 @@ CREATE TABLE raw_test (
 );
 
 -- RAW JSON TABLE
+DROP TABLE IF EXISTS raw_users;
 CREATE TABLE raw_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     raw_payload JSON NOT NULL,
@@ -24,6 +24,7 @@ CREATE TABLE raw_users (
 );
 
 -- CLEAN CSV TABLE
+DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id INT PRIMARY KEY,
     name VARCHAR(255),
@@ -36,17 +37,19 @@ CREATE TABLE test (
 );
 
 -- CLEAN USERS
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id CHAR(36) PRIMARY KEY,
     name VARCHAR(255),
     username VARCHAR(255),
-    dob DATE,
+    dob VARCHAR(255),
     address TEXT,
-    created_at TIMESTAMP NULL,
-    updated_at TIMESTAMP NULL
+    created_at VARCHAR(255),
+    updated_at VARCHAR(255)
 );
 
 -- TELEPHONE NUMBERS
+DROP TABLE IF EXISTS telephone_numbers;
 CREATE TABLE telephone_numbers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id CHAR(36),
@@ -55,13 +58,22 @@ CREATE TABLE telephone_numbers (
 );
 
 -- JOBS HISTORY
+DROP TABLE IF EXISTS jobs_history;
 CREATE TABLE jobs_history (
     job_id CHAR(36) PRIMARY KEY,
     user_id CHAR(36),
     occupation VARCHAR(255),
     is_fulltime BOOLEAN,
-    start DATE,
-    end DATE,
-    logged_at TIMESTAMP NULL,
+    start VARCHAR(255),
+    end VARCHAR(255),
+    logged_at VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
+
+
+
+CREATE USER 'boost_user'@'%' IDENTIFIED BY 'boost_password';
+GRANT ALL PRIVILEGES ON boost_test.* TO 'boost_user'@'%';
+FLUSH PRIVILEGES;
+
+SELECT user, host FROM mysql.user;
