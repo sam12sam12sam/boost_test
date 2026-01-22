c9fb3ce0b8b19be50348327090656e2f2fb02c6f
Sambireddy Puli <82464751+sam12sam12sam@users.noreply.github.com>
2026-01-22 09:43:07 +0530

    Update schema.sql

--- Etl_Pipeline/sql/schema.sql
+++ Etl_Pipeline/sql/schema.sql
@@ -2,8 +2,16 @@
 DROP SCHEMA IF EXISTS boost_test;
 CREATE SCHEMA boost_test;
 use boost_test;
+
+-- dropping tables
+-- DROP TABLE IF EXISTS jobs_history;
+-- DROP TABLE IF EXISTS telephone_numbers;
+-- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS raw_users;
+-- DROP TABLE IF EXISTS test;
+-- DROP TABLE IF EXISTS raw_test;
+
 -- RAW CSV TABLE
-DROP TABLE IF EXISTS raw_test;
 CREATE TABLE raw_test (
     id VARCHAR(20),
     name VARCHAR(255),
@@ -16,7 +24,6 @@ CREATE TABLE raw_test (
 );
 
 -- RAW JSON TABLE
-DROP TABLE IF EXISTS raw_users;
 CREATE TABLE raw_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     raw_payload JSON NOT NULL,
@@ -24,32 +31,29 @@ CREATE TABLE raw_users (
 );
 
 -- CLEAN CSV TABLE
-DROP TABLE IF EXISTS test;
 CREATE TABLE test (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     address TEXT,
     color VARCHAR(50),
-    created_at TIMESTAMP NULL,
-    last_login TIMESTAMP NULL,
+    created_at DATETIME NULL,
+    last_login DATETIME NULL,
     is_claimed BOOLEAN,
     paid_amount NUMERIC(12,2)
 );
 
 -- CLEAN USERS
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     user_id CHAR(36) PRIMARY KEY,
     name VARCHAR(255),
     username VARCHAR(255),
-    dob VARCHAR(255),
+    dob DATE,
     address TEXT,
-    created_at VARCHAR(255),
-    updated_at VARCHAR(255)
+    created_at DATETIME NULL,
+    updated_at DATETIME NULL
 );
 
 -- TELEPHONE NUMBERS
-DROP TABLE IF EXISTS telephone_numbers;
 CREATE TABLE telephone_numbers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id CHAR(36),
@@ -58,15 +62,14 @@ CREATE TABLE telephone_numbers (
 );
 
 -- JOBS HISTORY
-DROP TABLE IF EXISTS jobs_history;
 CREATE TABLE jobs_history (
     job_id CHAR(36) PRIMARY KEY,
     user_id CHAR(36),
     occupation VARCHAR(255),
-    is_fulltime VARCHAR(255),
-    start VARCHAR(255),
-    end VARCHAR(255),
-    logged_at VARCHAR(255),
+    is_fulltime BOOLEAN,
+    start DATE,
+    end DATE,
+    logged_at DATETIME NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
