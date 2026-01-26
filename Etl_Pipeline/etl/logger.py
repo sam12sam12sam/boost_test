f0bc95605d04dbf3b03170d8c403f5036e583319
Sambireddy Puli <82464751+sam12sam12sam@users.noreply.github.com>
2026-01-26 10:39:43 +0530

    Update logger.py

--- Etl_Pipeline/etl/logger.py
+++ Etl_Pipeline/etl/logger.py
@@ -2,7 +2,8 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    force=True
 )
 
 logger = logging.getLogger(__name__)
